1d22e47edfa5f6869140800a2c85c2ecefacab7c
agnesanka <cegielska.agnieszka@gmail.com>
2025-04-15 10:00:57 +0200

    dashboard redirect

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,9 @@ use Inertia\Inertia;
 
 Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
 
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
