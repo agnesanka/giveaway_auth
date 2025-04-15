4cf4fd671bcefe9febc202b26e3d55c306ae2623
agnesanka <cegielska.agnieszka@gmail.com>
2025-04-15 09:48:38 +0200

    Using AppHeaderLayout to display navbar on  home

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -38,32 +38,12 @@ const activeItemStyles = computed(
     () => (url: string) => (isCurrentRoute.value(url) ? 'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100' : ''),
 );
 
-const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-];
-
-const rightNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
-    },
-];
 </script>
 
 <template>
     <div>
-        <div class="border-b border-sidebar-border/80">
-            <div class="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
+        <div class="border-b border-sidebar-border/80 bg-white">
+            <div class="mx-auto flex h-16 items-center px-4 w-full">
                 <!-- Mobile Menu -->
                 <div class="lg:hidden">
                     <Sheet>
@@ -78,18 +58,6 @@ const rightNavItems: NavItem[] = [
                                 <AppLogoIcon class="size-6 fill-current text-black dark:text-white" />
                             </SheetHeader>
                             <div class="flex h-full flex-1 flex-col justify-between space-y-4 py-6">
-                                <nav class="-mx-3 space-y-1">
-                                    <Link
-                                        v-for="item in mainNavItems"
-                                        :key="item.title"
-                                        :href="item.href"
-                                        class="flex items-center gap-x-3 rounded-lg px-3 py-2 text-sm font-medium hover:bg-accent"
-                                        :class="activeItemStyles(item.href)"
-                                    >
-                                        <component v-if="item.icon" :is="item.icon" class="h-5 w-5" />
-                                        {{ item.title }}
-                                    </Link>
-                                </nav>
                                 <div class="flex flex-col space-y-4">
                                     <a
                                         v-for="item in rightNavItems"
@@ -113,53 +81,8 @@ const rightNavItems: NavItem[] = [
                 </Link>
 
                 <!-- Desktop Menu -->
-                <div class="hidden h-full lg:flex lg:flex-1">
-                    <NavigationMenu class="ml-10 flex h-full items-stretch">
-                        <NavigationMenuList class="flex h-full items-stretch space-x-2">
-                            <NavigationMenuItem v-for="(item, index) in mainNavItems" :key="index" class="relative flex h-full items-center">
-                                <Link :href="item.href">
-                                    <NavigationMenuLink
-                                        :class="[navigationMenuTriggerStyle(), activeItemStyles(item.href), 'h-9 cursor-pointer px-3']"
-                                    >
-                                        <component v-if="item.icon" :is="item.icon" class="mr-2 h-4 w-4" />
-                                        {{ item.title }}
-                                    </NavigationMenuLink>
-                                </Link>
-                                <div
-                                    v-if="isCurrentRoute(item.href)"
-                                    class="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-black dark:bg-white"
-                                ></div>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                </div>
 
                 <div class="ml-auto flex items-center space-x-2">
-                    <div class="relative flex items-center space-x-1">
-                        <Button variant="ghost" size="icon" class="group h-9 w-9 cursor-pointer">
-                            <Search class="size-5 opacity-80 group-hover:opacity-100" />
-                        </Button>
-
-                        <div class="hidden space-x-1 lg:flex">
-                            <template v-for="item in rightNavItems" :key="item.title">
-                                <TooltipProvider :delay-duration="0">
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <Button variant="ghost" size="icon" as-child class="group h-9 w-9 cursor-pointer">
-                                                <a :href="item.href" target="_blank" rel="noopener noreferrer">
-                                                    <span class="sr-only">{{ item.title }}</span>
-                                                    <component :is="item.icon" class="size-5 opacity-80 group-hover:opacity-100" />
-                                                </a>
-                                            </Button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>{{ item.title }}</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                            </template>
-                        </div>
-                    </div>
 
                     <DropdownMenu>
                         <DropdownMenuTrigger :as-child="true">
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,6 +3,7 @@ import CreatePost from '@/components/app/CreatePost.vue';
 import FollowingList from '@/components/app/FollowingList.vue';
 import GroupList from '@/components/app/GroupList.vue';
 import PostList from '@/components/app/PostList.vue';
+import AppHeaderLayout from '@/layouts/app/AppHeaderLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 </script>
 
@@ -12,16 +13,19 @@ import { Head, Link } from '@inertiajs/vue3';
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
+
+    <AppHeaderLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList />
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <CreatePost />
+                <PostList class="flex-1" />
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList />
-        </div>
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <CreatePost />
-            <PostList class="flex-1"/>
-        </div>
-    </div>
+    </AppHeaderLayout>
 </template>
